26649308040aa984901cec2ed5a132a5f569427e
peter <pngugi.wainaina@gmail.com>
2024-01-15 22:03:08 +0300

    lab done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+
+    for i in range(length):
+        if i == 0:
+            fibonacci_sequence.append(0)
+        elif i == 1:
+            fibonacci_sequence.append(1)
+        else:
+            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence)
